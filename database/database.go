39b883abcf133e3ee32b0e0c49e65aa0c1d05672
mosluce <mosluce@gmail.com>
2018-01-19 16:10:09 +0800

    update database tool

--- database/database.go
+++ database/database.go
@@ -19,15 +19,20 @@ type ConnectionConfig struct {
 	Dbname   string
 }
 
+const SQLITE = "sqlite3"
+const POSTGRES = "postgres"
+const MSSQL = "mssql"
+const MYSQL = "mysql"
+
 func (cc ConnectionConfig) URI() (uri string) {
 
-	if cc.Dialect == "mysql" {
+	if cc.Dialect == MYSQL {
 		uri = fmt.Sprintf("%s:%s@%s/%s?charset=utf8&parseTime=True&loc=Local", cc.User, cc.Pass, cc.Host, cc.Dbname)
-	} else if cc.Dialect == "postgres" {
+	} else if cc.Dialect == POSTGRES {
 		uri = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", cc.Host, cc.User, cc.Dbname, cc.Pass)
-	} else if cc.Dialect == "sqlite3" {
+	} else if cc.Dialect == SQLITE {
 		uri = cc.Filepath
-	} else if cc.Dialect == "mssql" {
+	} else if cc.Dialect == MSSQL {
 		uri = fmt.Sprintf("sqlserver://%s:%s@%s?database=%s", cc.User, cc.Pass, cc.Host, cc.Dbname)
 	}
 
